bbf64e6952077574cc3a2bc8c7269556fc71fad8
Thomas Domingo <tdomingo90@gmail.com>
2020-12-08 16:21:54 -0800

    make clicking search bar pass up query object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import VideoList from './VideoList.js';
 import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
-
+import YOUTUBE_API_KEY from '../config/youtube.js';
+import searchYouTube from '../lib/searchYouTube.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,8 +13,19 @@ class App extends React.Component {
       currentVideo: exampleVideoData[0]
     };
     this.clickHandler = this.clickHandler.bind(this);
+    this.searchHandler = this.searchHandler.bind(this);
   }
 
+  searchHandler(query, max = 5) {
+    let params = {
+      query,
+      max,
+      key: YOUTUBE_API_KEY
+    };
+    // console.log('searchHandler clicked', searchYouTube);
+    console.log('params: ', params);
+    // searchYoutube(params, () => console.log(data))
+  }
 
   clickHandler(index) {
     this.setState({
@@ -26,12 +38,13 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            {/* <div><h5><Search /></h5></div> */}
-            <h1>yo</h1>
+            <Search searchHandler={this.searchHandler} />
+            <button onClick={searchYouTube}>Press here for searchHandler</button>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
+            <h1>Some change</h1>
             <VideoPlayer video={this.state.currentVideo}/>
           </div>
           <div className="col-md-5">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,9 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  const {query, max, key} = options;
+  console.log('searchYouTube is being invoked!');
+  console.log('query: ', query, 'max: ', max);
+  console.log('callback: ', callback);
+  $.get(`https://www.googleapis.com/youtube/v3/search?key=${key}&q=${query}&maxResults=${max}&videoEmbeddable=true&type=video`, ()=> callback(data));
 };
 
 export default searchYouTube;
